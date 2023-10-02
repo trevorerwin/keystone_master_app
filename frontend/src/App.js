52800a40545747ed1bb141862f6c2a5e06fdd38c
Trevor Erwin <37050317+trevorerwin@users.noreply.github.com>
2023-10-02 09:33:19 -0400

    Beginning React refactoring

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
